92eea9c3054e94f934c4658936849d09f9bec517
Ryan Pena <ryanspena@gmail.com>
2021-03-10 15:55:28 -0600

    4.3.11 add ability to update and delete tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,8 @@ var taskIdCounter = 0;
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 var pageContentEl = document.querySelector("#page-content");
+var tasksInProgressEl = document.querySelector("#tasks-in-progress");
+var tasksCompletedEl = document.querySelector("#tasks-completed");
 
 var taskFormHandler = function(event) {
     event.preventDefault();
@@ -119,6 +121,25 @@ var taskButtonHandler = function(event) {
     }
 };
 
+var taskStatusChangeHandler = function(event) {
+    console.log(event.target.value);
+// get the task item's id
+    var taskId = event.target.getAttribute("data-task-id");
+
+    var statusValue = event.target.value.toLowerCase();
+
+    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+
+    if (statusValue === "to do") {
+        tasksToDoEl.appendChild(taskSelected);
+      } else if (statusValue === "in progress") {
+        tasksInProgressEl.appendChild(taskSelected);
+      } else if (statusValue === "completed") {
+        tasksCompletedEl.appendChild(taskSelected);
+      }
+
+};
+
 var editTask = function(taskId) {
     console.log("editing task #" + taskId);
     
@@ -155,3 +176,5 @@ var deleteTask = function(taskId) {
 formEl.addEventListener("submit", taskFormHandler);
 
 pageContentEl.addEventListener("click", taskButtonHandler);
+
+pageContentEl.addEventListener("change", taskStatusChangeHandler);
